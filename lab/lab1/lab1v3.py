63d1097bf964e958d2b5bb7cdbb791e1b1717760
afanasevauliana <afanasevauliana946@gmail.com>
2026-02-12 11:54:36 +0300

    lab1v3

---
+++ lab/lab1/lab1v3.py
@@ -0,0 +1,14 @@
+import random
+import time
+import csv
+n = int(input("Введите длину массива n: "))
+N = [random.randint(-100, 100) for _ in range(n)]
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        summa2 = N[i]+N[j]
+        for k in range(j+1, n):
+            if summa2 + N[k] == 0:
+                cnt += 1
+
+print(cnt)
