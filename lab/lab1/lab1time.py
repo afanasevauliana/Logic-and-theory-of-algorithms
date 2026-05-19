63f04b1547e526bd1c296a902fdef804a1dfc56e
afanasevauliana <afanasevauliana946@gmail.com>
2026-05-20 01:11:03 +0300

    l1

--- lab/lab1/lab1time.py
+++ lab/lab1/lab1time.py
@@ -16,16 +16,23 @@ def f3(arr, n):
 
 def main():
     n = int(input("Введите длину массива n: "))
-    N = [random.randint(-100, 100) for _ in range(n)]
+    N = [random.randint(-100, 100) for p in range(n)]
+    start_time = time.time()
     cnt3, arr3 = f3(N, n)
+    end_time = time.time()
     if len(N) < 50:
         print(f"Массив: {N}")
         print(f"Тройки: {arr3}")
     else:
         print(f"Массив: [{N[0]}, {N[1]}, {N[2]}, ..., {N[-3]}, {N[-2]}, {N[-1]}] (всего {len(N)} элементов)")
     print(f"Кол-во троек в массиве, сумма которых = 0: {cnt3}")
-    #for i in range(100, 200, 10):
+    print(f"Время выполнения алгоритма: {end_time - start_time}")
+    
 
+    print("Исследование временной сложности алгоритма:")
+    for q in range(10, 500, 10):
+        N = [random.randint(-100, 100) for p in range(q)]
+        cnt3, arr3 = f3(N, q)
     return 0
 
 if __name__ == "__main__":
